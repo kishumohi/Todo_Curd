2497fd56eb6ff0a1789e60f70b0427d8389c942e
kishan <kishankalavadiya@gmail.com>
2024-05-20 15:52:08 +0530

    change config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    SITE_URL: "http://localhost:3000/api/topics",
+    SITE_URL: "/api/topics",
     MONGODB_URI:
       "mongodb+srv://admin:admin@cluster0.lrb33ja.mongodb.net/crud_db",
   },
